c6cb061da22c14249ecab9f198495f7372a1a28a
github username <aslongman22@gmail.com>
2020-08-20 12:36:15 -0400

    email validation

--- Form-Validator/script.js
+++ Form-Validator/script.js
@@ -18,12 +18,39 @@ function showSuccess(input){
     formControl.className = 'form-control success';
 }
 
+//check valid email (javascript email regex)
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
 //Event listeners
 form.addEventListener('submit', function(e){
     e.preventDefault();
+
     if (username.value === '') {
         showError(username, 'Username is required');
     } else {
         showSuccess(username)
+    }
+
+    if (email.value === '') {
+        showError(email, 'Email is required');
+    } else if (!isValidEmail(email.value)) {
+        showError(email, 'Email is not valid');
+    } else {
+        showSuccess(email)
+    }
+
+    if (password.value === '') {
+        showError(password, 'Password is required');
+    } else {
+        showSuccess(password)
+    }
+
+    if (password2.value === '') {
+        showError(password2, 'Password 2 is required');
+    } else {
+        showSuccess(password2)
     }
 });
